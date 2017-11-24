d6fddca9339cdd456018e9f1e08e98af260b2ee4
ludvikkemp <ludvik11@ru.is>
2017-11-24 18:27:24 +0000

    add button added on main page for adding meetup

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import { Link } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navbar />
     <div className="container">
       <Main />
+    </div>
+    <div className="fixed-action-btn">
+      <Link to="/meetups/add" className="btn-floating btn-large red">
+        <i className="fa fa-plus"></i>
+      </Link>
     </div>
   </div>
 )
